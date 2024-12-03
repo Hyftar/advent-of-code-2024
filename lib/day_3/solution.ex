c40ad40aa381d58a233a750a52d2162e94cf2a75
Simon Landry <simon.landry@rumandcode.io>
2024-12-03 14:28:23 -0500

    Refactor day_3 solution

--- lib/day_3/solution.ex
+++ lib/day_3/solution.ex
@@ -2,27 +2,27 @@ defmodule Day3.Solution do
   def solve_question_1 do
     get_data()
     |> then(&Regex.scan(~r/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/, &1))
-    |> Enum.map(fn [_, x, y] -> {String.to_integer(x), String.to_integer(y)} end)
-    |> Enum.reduce(0, fn {x, y}, acc -> acc + x * y end)
+    |> Enum.map(fn [_, x, y] -> String.to_integer(x) * String.to_integer(y) end)
+    |> Enum.sum()
   end
 
   def solve_question_2 do
     get_data()
     |> then(&Regex.scan(~r/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)|do\(\)|don't\(\)/, &1))
-    |> Enum.reduce([0, "do()"], fn match, acc -> reducer(match, acc) end)
+    |> Enum.reduce([0, "do()"], &reducer/2)
     |> hd()
   end
 
-  def reducer([action], [sum, _] = _acc) do
+  defp reducer([action], [sum, _] = _acc) do
     [sum, action]
   end
 
-  def reducer([_, x, y], [sum, "do()"] = _acc) do
+  defp reducer([_, x, y], [sum, "do()"] = _acc) do
     {x, y} = {String.to_integer(x), String.to_integer(y)}
     [sum + x * y, "do()"]
   end
 
-  def reducer([_, _, _], [sum, "don't()"] = _acc) do
+  defp reducer([_, _, _], [sum, "don't()"] = _acc) do
     [sum, "don't()"]
   end
 
