bf8138881612a58d5b4d3f6322acfc23e9c7a9e6
Simon Landry <simon.landry@rumandcode.io>
2024-12-03 13:07:22 -0500

    Add day_3 question_3

--- lib/day_3/solution.ex
+++ lib/day_3/solution.ex
@@ -7,7 +7,23 @@ defmodule Day3.Solution do
   end
 
   def solve_question_2 do
-    nil
+    get_data()
+    |> then(&Regex.scan(~r/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)|do\(\)|don't\(\)/, &1))
+    |> Enum.reduce([0, "do()"], fn match, acc -> reducer(match, acc) end)
+    |> hd()
+  end
+
+  def reducer([action], [sum, _] = _acc) do
+    [sum, action]
+  end
+
+  def reducer([_, x, y], [sum, "do()"] = _acc) do
+    {x, y} = {String.to_integer(x), String.to_integer(y)}
+    [sum + x * y, "do()"]
+  end
+
+  def reducer([_, _, _], [sum, "don't()"] = _acc) do
+    [sum, "don't()"]
   end
 
   defp get_data do
