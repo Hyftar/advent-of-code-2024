155e6db15204f395d64670ad607de15aeb881bfa
Simon Landry <simon.landry@rumandcode.io>
2024-12-04 12:06:38 -0500

    Add day_4 part 2 solution

--- lib/day_4/solution.ex
+++ lib/day_4/solution.ex
@@ -14,6 +14,51 @@ defmodule Day4.Solution do
     |> Enum.sum()
   end
 
+  def solve_question_2 do
+    matrix =
+      get_data()
+      |> String.split("\n")
+      |> Enum.map(
+        fn x ->
+          x
+          |> String.trim()
+          |> String.codepoints()
+        end)
+      |> Enum.with_index()
+      |> Enum.map(fn {x, i} -> Enum.with_index(x) |> Enum.map(fn {y, j} -> {j, y} end) |> Enum.into(Map.new()) |> then(fn y -> {i, y} end) end)
+      |> Enum.into(Map.new())
+
+    0..((matrix |> Map.keys() |> Enum.count()) - 1)
+    |> Enum.map(
+      fn i ->
+        0..((matrix[i] |> Map.keys() |> Enum.count()) - 1)
+        |> Enum.count(
+          fn j ->
+            get_diagonals(matrix, i, j)
+            |> Enum.map(&Enum.join/1)
+            |> Enum.count(fn x -> Regex.match?(~r/MAS|SAM/, x) end)
+            |> then(fn x -> x == 2 end)
+          end)
+      end
+    )
+    |> Enum.sum()
+  end
+
+  def get_diagonals(matrix, middle_row, middle_col) do
+    [
+      [
+        if middle_row > 0 && middle_col > 0 do matrix[middle_row - 1][middle_col - 1] else "" end,
+        matrix[middle_row ][middle_col],
+        if middle_row < ((matrix |> Map.keys() |> Enum.count()) - 1) && middle_col < (matrix[middle_row] |> Map.keys() |> Enum.count() )- 1 do matrix[middle_row + 1][middle_col + 1] else "" end
+      ],
+      [
+        if middle_row < (matrix |> Map.keys() |> Enum.count()) - 1 && middle_col > 0 do matrix[middle_row + 1][middle_col - 1] else "" end,
+        matrix[middle_row ][middle_col],
+        if middle_row > 0 && middle_col < (matrix[middle_row] |> Map.keys() |> Enum.count()) - 1 do matrix[middle_row - 1][middle_col + 1] else "" end
+      ]
+    ]
+  end
+
   def rotations(list) do
     [
       list,
