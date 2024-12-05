a152ff68892aa7f84ee510bc364b95f9851b7de8
Hyftar <landry_simon@pm.me>
2024-12-05 15:03:19 -0500

    Add day_5 part_2 solution

--- lib/day_5/solution.ex
+++ lib/day_5/solution.ex
@@ -1,6 +1,11 @@
 defmodule Day5.Solution do
   def solve_question_1 do
-    [raw_rules, print_queue] = get_data()
+    [raw_rules, print_queue] =
+      File.read!("lib/day_5/input.txt")
+      |> String.split("\n")
+      |> Enum.map(&String.trim/1)
+      |> Enum.filter(fn x -> x != "" end)
+      |> Enum.chunk_by(&(Regex.match?(~r/^\d+\|\d+/, &1)))
 
     parsed_rules =
       raw_rules
@@ -27,18 +32,41 @@ defmodule Day5.Solution do
           end
         )
       end)
-    |> Enum.map(fn x -> Enum.at(x, div(length(x), 2)) end)
+    |> Enum.map(&middle_element/1)
     |> Enum.sum()
   end
 
   def assign_default_rule(element, nil = _current), do: [element]
   def assign_default_rule(element, current), do: [element] ++ current
 
-  def get_data do
-    File.read!("lib/day_5/input.txt")
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.filter(fn x -> x != "" end)
-    |> Enum.chunk_by(&(Regex.match?(~r/^\d+\|\d+/, &1)))
+  def solve_question_2 do
+    [rules, print_queue] =
+      File.read!("lib/day_5/input.txt")
+      |> String.split("\n\n")
+
+    print_queue =
+      print_queue
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn x -> String.split(x, ",") |> Enum.map(&String.to_integer/1) end)
+
+    print_queue
+    |> Enum.map(
+      fn line ->
+        {
+          line,
+          Enum.sort_by(
+            line,
+            &Function.identity/1,
+            &(String.contains?(rules, "#{&1}|#{&2}"))
+          )
+        }
+      end)
+    |> Enum.filter(fn {line, sorted} -> line != sorted end)
+    |> Enum.map(fn {_line, sorted} -> middle_element(sorted) end)
+    |> Enum.sum()
+  end
+
+  defp middle_element(list) do
+    Enum.at(list, div(length(list), 2))
   end
 end
